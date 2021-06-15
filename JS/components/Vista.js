e3748df9e0ab2f5f0fdd7d9915438ab667def965
ChocoTortuga <v.godoy.muoz@gmail.com>
2021-06-15 13:18:39 -0400

    Detalles

--- JS/components/Vista.js
+++ JS/components/Vista.js
@@ -3,20 +3,20 @@ data.component('Vista', {
     /*html*/
     `
     <div class="row">
-            <div class="col-12 text-warning">
+            <div class="col-12 text-dark">
                 <h1 class="text-center"> Videojuegos agregados</h1>
             </div>
             <div class="row justify-content-center">
                 <div v-for="(item, index) in productos" :key="index" class="card list-group-item-warning text-center m-3" style="width: 18rem;">
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item list-group-item-warning">{{item.id}}</li>
-                        <li class="list-group-item list-group-item-warning"><img :src="item.img" class="img-fluid"></li>
-                        <li class="list-group-item list-group-item-warning">{{item.nombre}}</li>
-                        <li class="list-group-item list-group-item-warning">{{item.plataforma}}</li>
-                        <li class="list-group-item list-group-item-warning">{{item.genero}}</li>
-                        <li class="list-group-item list-group-item-warning">{{item.estado}}</li>
-                        <li class="list-group-item list-group-item-warning">{{item.precio}}</li>
-                        <li class="list-group-item list-group-item-warning">               
+                        <li class="list-group-item list-group-item-dark">{{item.id}}</li>
+                        <li class="list-group-item list-group-item-dark"><img :src="item.img" class="img-fluid"></li>
+                        <li class="list-group-item list-group-item-dark">{{item.nombre}}</li>
+                        <li class="list-group-item list-group-item-dark">{{item.plataforma}}</li>
+                        <li class="list-group-item list-group-item-dark">{{item.genero}}</li>
+                        <li class="list-group-item list-group-item-dark">{{item.estado}}</li>
+                        <li class="list-group-item list-group-item-dark">{{item.precio}}</li>
+                        <li class="list-group-item list-group-item-dark">               
                         <button @click="borrarProd(index); obtenerProductos();" class="btn btn-danger btn.lg m-1"><i class="icofont-ui-delete"></i></button>
                         <button @click="ActualizarDatos(index); obtenerProductos();" class="btn btn-success btn.lg m-1"><i class="icofont-ui-reply"></i></button>
                         </li>
@@ -33,7 +33,7 @@ data.component('Vista', {
     },
 
     computed: {
-        ...Vuex.mapState(['productos']),
+        ...Vuex.mapState(['productos'])
     },
 
     created () {
